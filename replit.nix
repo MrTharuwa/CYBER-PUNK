b625ad2b302c38628afffa9d6a29d1ccf0b6d35d
Kai...!! <achintya0071@gmail.com>
2023-11-25 01:34:38 +0530

    Replit

--- replit.nix
+++ replit.nix
@@ -1,9 +1,28 @@
 { pkgs }: {
-  deps = [
-    pkgs.nano
-    pkgs.nodejs-18_x
+	deps = [
+		pkgs.nano
+  pkgs.nodejs-18_x
     pkgs.nodePackages.typescript-language-server
     pkgs.yarn
     pkgs.replitPackages.jest
-  ];
+	];
+}
+
+{ pkgs }: {
+    deps = [
+        pkgs.ffmpeg
+        pkgs.git
+        pkgs.nodejs-19_x
+        pkgs.nodePackages.typescript
+        pkgs.libwebp
+        pkgs.python
+        pkgs.libuuid
+        pkgs.imagemagick
+        pkgs.pm2
+        pkgs.wget
+        pkgs.yarn
+    ];
+  env ={
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid];
+  };
 }
