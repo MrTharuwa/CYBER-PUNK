857b10c4da1e4ce7cfd4c19a7d1cb488bd3bcd73
Mʀ Tʜᴀʀᴜᴡᴀ X <153652249+MrTharuwa@users.noreply.github.com>
2024-03-07 19:24:54 +0530

    Add files via upload

--- Core.js
+++ Core.js
@@ -6808,6 +6808,7 @@ _Click the button below to download_`
         if (isBan) return reply(mess.banned);
         if (isBanChat) return reply(mess.bangc);
         A17.sendMessage(from, { react: { text: "✨", key: m.key } })
+        const helpmenu = `fs.readFileSync(`./Assets/audio/Alive.mp3`)
         const helpmenu = `Hellow *${pushname}* Dear...!! ${nowtime} ,
   
 *Hellow, I am "Cyber punk" Multi Device Whatsapp bot create by tharuwa*
