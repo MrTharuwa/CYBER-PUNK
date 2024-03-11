f5c20da58210a9cb781e07968dc53d7feba53d92
Mʀ Tʜᴀʀᴜᴡᴀ X <153652249+MrTharuwa@users.noreply.github.com>
2024-03-11 11:38:20 +0530

    Add files via upload

--- Core.js
+++ Core.js
@@ -655,7 +655,7 @@ Typed *surrender* to surrender and admited defeat`
     reply ('Cyber-punk-md-bot-creator')
   }
   
-  if (smallinput=='${prefix}ping') {
+  if (smallinput=='ping') {
       reply (`Pong ${latensie.toFixed(4)} ms`)
   }
   
@@ -694,7 +694,7 @@ Typed *surrender* to surrender and admited defeat`
       runtime: `Hey ${pushname}\n${nowtime}\n\nMy runtime:${runtime(process.uptime())}\n\nPrefix is: *${prefix}*\n\nTime: ${kaitime}\n\nDate: ${kaidate}\n\nToday is ${currentDay}`,
       konichiwa: `Konichiwa ${pushname}, I am ${BotName}. How can I help you?`,
       Tharuwa: 'Cyber-punk-md-bot-creator',
-  ${prefix}ping: `📍*Pong ${latensie.toFixed(4)} ms*`,
+      ping: `📍*Pong ${latensie.toFixed(4)} ms*`,
       'good morning': `Good morning to you too ${pushname} ☺️. Have a great day 😇.`,
       gm: `Good morning to you too ${pushname} ☺️. Have a great day 😇.`,
       'good afternoon': `Good afternoon to you too ${pushname} ✨. Wishing you an enjoyable afternoon too 😇🤞🏻.`,
